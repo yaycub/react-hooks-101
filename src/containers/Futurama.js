2a87b8d5e83041099c1386e4d17a4b73cf5c6cd9
Jacob Harrington <jharrington4@gmail.com>
2020-02-03 13:42:32 -0800

    getQuote fetch completed and wired with class container

--- src/containers/Futurama.js
+++ src/containers/Futurama.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import Quote from '../components/futurama/Quote';
 import Button from '../components/futurama/Button';
+import { getQuote } from '../services/getQuote';
 
 export default class Futurama extends Component{
+  state = { 
+    displayQuote: {
+      character: '',
+      quote: '',
+      image: ''
+    }
+  }
+
+  componentDidMount(){
+    this.fetch();
+  }
+
+  handleClick = () => {
+    this.fetch();
+  }
+
+  fetch = () => {
+    return getQuote()
+      .then(res => this.setState({ displayQuote: res }));
+  }
+
   render(){
+    const { displayQuote } = this.state;
+
     return (
       <>
-        <Quote />
-        <Button />
+        <Quote {...displayQuote} />
+        <Button onClick={this.handleClick} />
       </>
     );
   }
